0be6aca4da8ecdb5a32d3bd07c63cb2d711508fe
Ashok-2004 <manishmishra8970@gmail.com>
2026-02-10 18:44:49 +0530

    Fix Vercel server.js

--- src/server.js
+++ src/server.js
@@ -1,28 +1,6 @@
 require("dotenv").config();
 const createApp = require("./app");
-const logger = require("./utils/logger");
 
 const app = createApp();
 
 module.exports = app;
-
-if (process.env.VERCEL !== "production") {
-  const PORT = parseInt(process.env.PORT, 10) || 3000;
-
-  const tryListen = (port) => {
-    const server = app.listen(port, () => {
-      logger.info(`Server running on port ${port}`);
-    });
-    server.on("error", (err) => {
-      if (err.code === "EADDRINUSE" && port < 3010) {
-        logger.warn(`Port ${port} in use, trying ${port + 1}...`);
-        server.close();
-        tryListen(port + 1);
-      } else {
-        throw err;
-      }
-    });
-  };
-
-  tryListen(PORT);
-}
